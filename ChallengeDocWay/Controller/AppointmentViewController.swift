cc7580fa79fd16a8b315c6535ab7c770b90f32a9
Gustavo Quenca <gustavo.quenca@icloud.com>
2019-03-19 18:59:34 -0300

    Keyboard functionallity for Appointment View

--- ChallengeDocWay/Controller/AppointmentViewController.swift
+++ ChallengeDocWay/Controller/AppointmentViewController.swift
@@ -28,11 +28,34 @@ class AppointmentViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var symptomButton: UIButton!
     @IBOutlet weak var specialtyButton: UIButton!
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     private let dataSource = DataSource()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
+    
+    @objc func keyboardWillShow(notification:NSNotification){
+        var userInfo = notification.userInfo!
+        var keyboardFrame:CGRect = (userInfo[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        keyboardFrame = self.view.convert(keyboardFrame, from: nil)
+        
+        var contentInset:UIEdgeInsets = self.scrollView.contentInset
+        contentInset.bottom = keyboardFrame.size.height
+        scrollView.contentInset = contentInset
+    }
+    
+    @objc func keyboardWillHide(notification:NSNotification){
+        
+        let contentInset:UIEdgeInsets = UIEdgeInsets.zero
+        scrollView.contentInset = contentInset
+    }
+    
+
     
     override func viewWillAppear(_ animated: Bool) {
         
@@ -98,4 +121,3 @@ class AppointmentViewController: UIViewController, UITextFieldDelegate {
         }
     }
 }
-
